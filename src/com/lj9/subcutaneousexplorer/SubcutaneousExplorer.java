24936f1ff85d24d8cfa36d419486c66dbf864e00
trollkatt <lanjoe9@gmail.com>
2014-04-26 08:12:03 +0200

    Committ of initial classes
    Basic mjEngine boilerplate

---
+++ src/com/lj9/subcutaneousexplorer/SubcutaneousExplorer.java
@@ -0,0 +1,7 @@
+package com.lj9.subcutaneousexplorer;
+
+import android.app.Activity;
+
+public class SubcutaneousExplorer extends Activity {
+
+}
---
+++ src/com/lj9/subcutaneousexplorer/menu/MenuUniverseScene.java
@@ -0,0 +1,40 @@
+package com.lj9.subcutaneousexplorer.menu;
+
+import android.content.Context;
+import android.view.KeyEvent;
+
+import com.lj9.mjEngine.mjScene;
+
+public class MenuUniverseScene extends mjScene {
+
+	@Override
+	public void OnEnter(Context context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnUpdate(float t_elapsed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDraw(float t_elapsed, StereoProjection stereoParam) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnProcessKey(KeyEvent keyEvent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnLeave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
